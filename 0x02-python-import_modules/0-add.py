19690a8e2210892df8313190511502f5f971edba
jexjatto <jextrim@gmail.com>
2022-12-21 01:17:35 +0100

    redone import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 0_add.py
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
@@ -8,4 +7,3 @@ if __name__ == "__main__":
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-    
